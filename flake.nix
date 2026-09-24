2c3081345b86a7acc784ecd549dcafc9a3dbc7de
Aaron Bushnell <aabushnell@gmail.com>
2026-09-24 10:26:39 +0200

    initial

---
+++ flake.nix
@@ -0,0 +1,58 @@
+{
+  description = "NET7212 dev shell";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    fenix.url = "github:nix-community/fenix";
+    fenix.inputs.nixpkgs.follows = "nixpkgs";
+  };
+
+  outputs =
+    { nixpkgs, fenix, ... }:
+    let
+      systems = [
+        "x86_64-linux"
+        "aarch64-darwin"
+      ];
+      forAllSystems = nixpkgs.lib.genAttrs systems;
+    in
+    {
+      devShells = forAllSystems (
+        system:
+        let
+          pkgs = nixpkgs.legacyPackages.${system};
+          inherit (pkgs.lib) optionals;
+          inherit (pkgs.stdenv) isLinux isDarwin;
+
+          rust = fenix.packages.${system}.stable.toolchain;
+        in
+        {
+          default = pkgs.mkShell {
+            packages =
+              with pkgs;
+              [
+                # C toolchain
+                gcc
+                clang
+                clang-tools
+                gnumake
+                bats
+                check
+
+                # rust
+                rust
+              ]
+              ++ optionals isLinux [
+                gdb
+                strace
+                ltrace
+                valgrind
+              ]
+              ++ optionals isDarwin [
+                lldb
+              ];
+          };
+        }
+      );
+    };
+}
